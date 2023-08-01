ef44464f040f137a83515dbfa61fc2769b8b929d
jahanus <hanus.j@icloud.com>
2023-08-01 23:30:28 +0200

    Initial commit - cdk install - CRUD lambdas - dynamo

---
+++ lib/dynamodb-crud-stack.ts
@@ -0,0 +1,110 @@
+import { Stack, StackProps, CfnOutput, RemovalPolicy } from "aws-cdk-lib"
+import { Construct } from "constructs"
+import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Architecture, Runtime } from 'aws-cdk-lib/aws-lambda';
+
+export class DynamodbCrudStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    const todoTable = new Table(this, 'todoTable', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: RemovalPolicy.DESTROY
+    })
+
+    todoTable.addGlobalSecondaryIndex({
+      indexName: 'ownerIndex',
+      partitionKey: {
+        name: 'owner',
+        type: AttributeType.STRING
+      }
+    })
+
+    new CfnOutput(this, 'todoTableName', {
+      value: todoTable.tableName
+    })
+
+    const createTodoFn = new NodejsFunction(this, 'createTodoFn', {
+      runtime: Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda-fns/create/index.ts`,
+      handler: 'createTodo',
+      architecture: Architecture.X86_64,
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName
+      }
+    })
+
+    todoTable.grantReadWriteData(createTodoFn)
+
+    const getAllTodoFn = new NodejsFunction(this, 'getAllTodoFn', {
+      runtime: Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda-fns/getAll/index.ts`,
+      handler: 'getAll',
+      architecture: Architecture.X86_64,
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName
+      }
+    })
+
+    todoTable.grantReadData(getAllTodoFn)
+
+    const getOneTodoFn = new NodejsFunction(this, 'getOneTodoFn', {
+      runtime: Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda-fns/getOne/index.ts`,
+      handler: 'getOne',
+      architecture: Architecture.X86_64,
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName
+      }
+    })
+
+    todoTable.grantReadData(getOneTodoFn)
+
+    const updateTodoFn = new NodejsFunction(this, 'updateTodoFn', {
+      runtime: Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda-fns/update/index.ts`,
+      handler: 'update',
+      architecture: Architecture.X86_64,
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName
+      }
+    })
+
+    todoTable.grantReadWriteData(updateTodoFn)
+
+    const deleteTodoFn = new NodejsFunction(this, 'deleteTodoFn', {
+      runtime: Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda-fns/delete/index.ts`,
+      handler: 'deleteTodo',
+      architecture: Architecture.X86_64,
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName
+      }
+    })
+
+    todoTable.grantReadWriteData(deleteTodoFn)
+
+    const tableWithIndex = Table.fromTableAttributes(this, 'tableWithIndex', {
+      tableName: todoTable.tableName,
+      globalIndexes: ['ownerIndex']
+    })
+
+    const queryTodoFn = new NodejsFunction(this, 'queryTodoFn', {
+      runtime: Runtime.NODEJS_16_X,
+      entry: `${__dirname}/../lambda-fns/query/index.ts`,
+      handler: 'queryTodo',
+      architecture: Architecture.X86_64,
+      environment: {
+        TODO_TABLE_NAME: todoTable.tableName
+      }
+    })
+
+    tableWithIndex.grantReadData(queryTodoFn)
+  }
+}
